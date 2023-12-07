1cbf1e5369fdaa32ece8563eb74fa758006eba60
Seokhun-Yang <ddayang1234@gmail.com>
2023-12-08 01:39:44 +0900

    초음파 센서 인식 후 텔레그램으로 알림 전송

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,26 @@
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 #include <../Secrets.h>
+#include <NewPing.h>
 
-String server = "api.telegram.org";
-String message = "Hello!";
+#define MAX_DISTANCE 10
 
-void setup()
-{
+struct SensorLocation {
+  NewPing sensor;
+  String location;
+};
+
+// Define sensor and location pairs
+SensorLocation sensorLocations[] = {
+  {NewPing(12, 13, MAX_DISTANCE), "창고"}, // (trigger_pin, echo_pin, maximum_distance)
+  {NewPing(16, 17, MAX_DISTANCE), "복도"},
+  {NewPing(18, 19, MAX_DISTANCE), "창문"},
+  {NewPing(26, 27, MAX_DISTANCE), "현관문"}
+};
+
+void sendTelegramNotification(String location);
+
+void setup() {
   Serial.begin(115200);
 
   WiFi.begin(getWifiSSID(), getWifiPassWord());
@@ -20,31 +34,42 @@ void setup()
   Serial.println("Connected to WiFi");
 }
 
-void loop()
-{
+void loop() {
   if ((WiFi.status() == WL_CONNECTED)) {
 
-    WiFiClientSecure httpsClient;
-    HTTPClient http;
+    for (int i = 0; i < 4; i++) {
+      delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 
-    String url = "/bot" + getToken() + "/sendMessage?chat_id=" + getChatID() + "&text=" + message;
-    httpsClient.setInsecure(); // SSL 인증서 검증 비활성화
-    http.begin(httpsClient, "https://" + server + url); // HTTPS 연결 설정
+      unsigned int uS = sensorLocations[i].sensor.ping(); // Send ping, get ping time in microseconds (uS).
 
-    Serial.print("[HTTPS] GET... ");
-    int httpCode = http.GET(); // HTTP 요청 수행
-
-    if (httpCode > 0) {
-      String payload = http.getString(); // HTTP 응답 가져오기
-      Serial.println("HTTP Status code: " + String(httpCode));
-      Serial.println("HTTP Response: " + payload);
+      if (uS != NO_ECHO) { // if there's an echo
+        sendTelegramNotification(sensorLocations[i].location);
+      }
     }
-    else {
-      Serial.printf("[HTTPS] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-    }
+  }
+}
 
-    http.end(); // HTTP 연결 종료
+void sendTelegramNotification(String location) {
+  WiFiClientSecure httpsClient;
+  HTTPClient http;
+
+  String message = location + "에서 수상한 움직임이 감지되었습니다!";
+  String url = "/bot" + getToken() + "/sendMessage?chat_id=" + getChatID() + "&text=" + message;
+  
+  httpsClient.setInsecure(); // SSL 인증서 검증 비활성화
+  http.begin(httpsClient, "https://api.telegram.org" + url); // HTTPS 연결 설정
+
+  Serial.print("[HTTPS] GET... ");
+  int httpCode = http.GET(); // HTTP 요청 수행
+
+  if (httpCode > 0) {
+    String payload = http.getString(); // HTTP 응답 가져오기
+    Serial.println("HTTP Status code: " + String(httpCode));
+    Serial.println("HTTP Response: " + payload);
+  }
+  else {
+    Serial.printf("[HTTPS] GET... failed, error: %s", http.errorToString(httpCode).c_str());
   }
 
-  delay(10000);
+  http.end(); // HTTP 연결 종료
 }
